32c9980e2bfcdf0ceffd31ef3ef0fbe7f4533463
Tom Close <tom.g.close@gmail.com>
2024-04-17 10:31:17 +1000

    initial commit of generated stubs

---
+++ related-packages/fileformats-extras/fileformats/extras/medimage_nireports/__init__.py
@@ -0,0 +1,8 @@
+from ._version import __version__  # noqa: F401
+from pathlib import Path
+import typing as ty
+from random import Random
+from fileformats.core import FileSet, SampleFileGenerator
+from fileformats.medimage_nireports import (
+)
+
---
+++ related-packages/fileformats-extras/fileformats/extras/medimage_nireports/tests/test_generate_sample_data.py
@@ -0,0 +1,4 @@
+import pytest
+from fileformats.medimage_nireports import (
+)
+
